f0515851ba6b6d3607b55028b9d5b2f9964576ff
Ian <62034540+i13e@users.noreply.github.com>
2022-02-16 15:45:42 -0500

    Update autosnap-wrapper.sh

--- autosnap-wrapper.sh
+++ autosnap-wrapper.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/bash
+# authors: foundobjects & i13e 
 #
-# timeshift-autosnap helper for your AUR helper (autosnap-wrapper.sh)
-#
-# this helper code stops your AUR helper + timeshift-autosnap from taking multiple snapshots during the same update
+# this script prevents timeshift-autosnap from taking multiple snapshots 
+# during the same update while using a supported AUR helper
+
+# Automatically set which AUR helper to use
+if [ -n "$AUR_HELPER" ] ; then
+    echo "AUR_HELPER has been manually set to $AUR_HELPER by the user" # allow manually setting the AUR helper to be used
+elif [ -x /usr/bin/paru ] ; then
+    export AUR_HELPER='/usr/bin/paru' # if both are installed preference goes to paru
+elif [ -x /usr/bin/yay ] ; then
+    export AUR_HELPER='/usr/bin/yay' 
+else
+    echo "warning: please install a supported aur helper (paru or yay) to use this script." >&2
+fi
 
 # shellcheck disable=SC2155
 export __AUTOSNAP_LOCK="$(mktemp -ut "aur-helper-autosnap.lock-XXXXXXX")"
